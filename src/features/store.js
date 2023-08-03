c002b3d028b5a21dec76e26f6e390269d614c92b
Dmytro <dmytro.kudriavtsev88@gmail.com>
2023-08-03 22:22:33 +0200

    create prodcut page

--- src/features/store.js
+++ src/features/store.js
@@ -1,11 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import categoriesSlice from './categories/categoriesSlice';
-import productsSlice from './products/productsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./api/apiSlice";
+
+import categoriesSlice from "./categories/categoriesSlice";
+import productsSlice from "./products/productsSlice";
 
 export const store = configureStore({
   reducer: {
     categories: categoriesSlice,
     products: productsSlice,
-    devtools: true
-  }
-})
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getMiddleware) => getMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
